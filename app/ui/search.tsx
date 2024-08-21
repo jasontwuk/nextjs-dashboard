21c135db8273e86f05d9a8f37816f38572f3432e
Jason <jasontwuk@gmail.com>
2024-08-21 15:26:35 +0100

    ch11 - add search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,32 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    // console.log(`Searching... ${term}`);
+
+    // *** Note: URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters.
+    const params = new URLSearchParams(searchParams);
+
+    // *** Note: when the users starting a new search, bring them to the first page of the new search result
+    params.set("page", "1");
+
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+
+    replace(`${pathName}?${params.toString()}`);
+  }, 3000);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +35,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
