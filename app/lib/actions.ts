ef8de6a413ad2af83ee01190a8af0fb60b0f10af
Jason <jasontwuk@gmail.com>
2024-08-22 18:18:31 +0100

    ch13 - 1 - fix a typo

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -112,7 +112,7 @@ export async function deleteInvoice(id: string) {
   // throw new Error("Failed to Delete Invoice");
 
   try {
-    await sql`DELETE FROM invoices WHERE idd = ${id}`;
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
 
     // *** Note: since this action is being called in the /dashboard/invoices path, you don't need to call redirect. Calling revalidatePath will trigger a new server request and re-render the table.
     revalidatePath("/dashboard/invoices");
