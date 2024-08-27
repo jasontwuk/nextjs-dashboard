5dd067a33a95c831bf3b33d87acec9b83f614ec4
Jason <jasontwuk@gmail.com>
2024-08-27 11:05:11 +0100

    ch16 - add metadata (page title and description)

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,6 +3,12 @@ import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { notFound } from "next/navigation";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Edit Invoice",
+};
+
 // *** Note: use the "params" prop to access the dynamic route's id (the [id] folder above "edit" folder)
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
